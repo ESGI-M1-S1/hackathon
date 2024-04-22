26958f295fb67823a88c95b990868d75350aeb14
Côme Bonal <come.bonal@gmail.com>
2024-04-22 17:06:08 +0200

    Adding of the  calendar component

---
+++ frontend/app/calendar/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { MonthlyCalendar } from "@/app/calendar/components/MonthlyCalendar"
+
+const Page = () => {
+  return (
+    <MonthlyCalendar></MonthlyCalendar>
+  );
+};
+
+export default Page;
