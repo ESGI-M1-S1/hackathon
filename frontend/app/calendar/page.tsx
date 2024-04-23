25466c3a0b4478d0d35594884fed8042dffa13b3
Côme Bonal <come.bonal@gmail.com>
2024-04-23 11:42:12 +0200

    Adding of the calendar and main components button etc...

--- frontend/app/calendar/page.tsx
+++ frontend/app/calendar/page.tsx
@@ -1,12 +1,17 @@
-"use client";
+"use client"
 import React from "react";
 import Link from "next/link";
+import Card from '@/components/ui/card';
+import NavBar from '@/components/navbar';
+
 import { MonthlyCalendar } from "@/app/calendar/components/MonthlyCalendar"
 
 const Page = () => {
-  return (
-    <MonthlyCalendar></MonthlyCalendar>
-  );
+    return (
+        <section>
+            <MonthlyCalendar></MonthlyCalendar>
+        </section>
+    );
 };
 
 export default Page;
