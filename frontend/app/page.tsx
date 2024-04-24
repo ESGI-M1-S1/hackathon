8d60757d0d2ab27fc6826e071d063dfb90d7c396
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2024-04-24 10:31:26 +0200

    homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <main>
       <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
         <div className="flex flex-col items-center gap-8 px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-32">
           <h1 className="mb-4 text-4xl leading-none text-white md:text-5xl lg:text-6xl">
@@ -25,38 +25,41 @@ export default function Home() {
           </p>
         </div>
       </section>
-      <section className="py-12">
-        <Card
-          imgSrc={
-            "https://images.unsplash.com/photo-1497215728101-856f4ea42174?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          }
-          className="max-w-sm"
-        >
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Atelier dégustation
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-          <Button>
-            Read more
-            <svg
-              className="-mr-1 ml-2 h-4 w-4"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </Button>
-        </Card>
+      <section className="py-4 my-8 flex justify-between max-w-screen-lg mx-auto gap-6">
+        {[0, 0, 0].map((_, index) => (
+          <Card
+            key={index}
+            imgSrc={
+              "https://images.unsplash.com/photo-1497215728101-856f4ea42174?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            }
+            className="max-w-sm"
+          >
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Atelier dégustation
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+            <Button>
+              Read more
+              <svg
+                className="-mr-1 ml-2 h-4 w-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </Button>
+          </Card>
+        ))}
       </section>
-      <section>
+      <section className={"flex flex-col gap-5 mx-auto max-w-screen-lg"}>
         <h3 className={"mb-4 text-3xl leading-none text-center"}>
           Rétrospective des anciens ateliers
         </h3>
@@ -87,6 +90,23 @@ export default function Home() {
           </p>
         </article>
       </section>
-    </>
+      <section
+        className={
+          "flex flex-col justify-between gap-8 items-center bg-pink-950 text-white my-8 py-24"
+        }
+      >
+        <div className={"max-w-screen-lg flex flex-col gap-3"}>
+          <h3 className={"mb-4 text-3xl font-bold leading-none text-center"}>
+            Venez découvrir nos ateliers
+          </h3>
+          <p className={"text-xl"}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation
+          </p>
+        </div>
+        <Button>Découvrir les ateliers →</Button>
+      </section>
+    </main>
   );
 }
