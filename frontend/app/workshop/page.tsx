93836d788b20f25fa004cd13de45f66eeb77cd55
mateocarciu <carciu98@gmail.com>
2024-04-25 16:21:52 +0200

    caards done

--- frontend/app/workshop/page.tsx
+++ frontend/app/workshop/page.tsx
@@ -55,9 +55,9 @@ const cardsData = [
 
 const Page = () => {
     return (
-        <div className="flex flex-col items-center">
-            <h1 className="text-3xl font-bold mb-4 mx-6">Participer aux ateliers</h1>
-            <p className="text-lg mb-4 mx-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, repudiandae necessitatibus</p>
+        <div className="flex flex-col items-center bg-black pb-3">
+            <h1 className="text-3xl font-bold mb-4 mx-6 text-brownText">Participer aux ateliers</h1>
+            <p className="text-lg mb-4 mx-6 text-brownText">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, repudiandae necessitatibus</p>
             <div className="flex flex-wrap justify-center gap-6">
                 {cardsData.map((card, index) => (
                     <Card key={index} href={`/workshop/${index + 1}`} imageUrl={card.imgSrc} title={card.title} description={card.description} className='max-w-xs max-h-xs mx-6'></Card>
