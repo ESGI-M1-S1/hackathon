903022b5868979d2b516ec13bc46e6843bdf9b14
antoine-falgiglio <antoine.falgiglio@bedrockstreaming.com>
2024-04-25 16:22:09 +0200

    input

---
+++ frontend/app/components/Input.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const InputField = () => (
+  <input
+    name="firstName"
+    placeholder="input text"
+    className="border-brown border-2 bg-transparent h-10 mb-2 pl-4 pr-4 placeholder-white"
+  />
+);
+
+export default InputField;
