a83ce71cbe832d7993a4516eb85bfd43bbaa47e4
mateocarciu <carciu98@gmail.com>
2024-04-25 15:15:11 +0200

    push

--- frontend/app/workshop/page.tsx
+++ frontend/app/workshop/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, Pagination } from 'flowbite-react';
+import { Pagination } from 'flowbite-react';
 import Link from 'next/link';
+import Card from '../../components/ui/card';
 
 const cardsData = [
     {
@@ -59,22 +60,7 @@ const Page = () => {
             <p className="text-lg mb-4 mx-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, repudiandae necessitatibus</p>
             <div className="flex flex-wrap justify-center gap-6">
                 {cardsData.map((card, index) => (
-                    <Card key={index} imgSrc={card.imgSrc} className='max-w-xs max-h-xs mx-6'>
-                        <div className="p-5">
-                            <Link href={`/workshop/${index + 1}`} passHref>
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{card.title}</h5>
-                            </Link>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{card.description}</p>
-                            <Link href={`/workshop/${index + 1}`} passHref>
-                                <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    Read more
-                                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                    </svg>
-                                </div>
-                            </Link>
-                        </div>
-                    </Card>
+                    <Card key={index} href={`/workshop/${index + 1}`} imageUrl={card.imgSrc} title={card.title} description={card.description} className='max-w-xs max-h-xs mx-6'></Card>
                 ))}
             </div>
         </div>
--- frontend/components/ui/card.tsx
+++ frontend/components/ui/card.tsx
@@ -1,25 +1,46 @@
 import React from 'react';
-import ButtonArrow from "./buttonArrow"
+import Link from 'next/link';
+
 interface CardProps {
+    href: string;
     title: string;
     description: string;
     imageUrl: string;
 }
 
-const Card: React.FC<CardProps> = ({ title, description, imageUrl }) => {
+const Card: React.FC<CardProps> = ({ href, title, description, imageUrl }) => {
     return (
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="max-w-xs bg-white border border-orange-400 shadow dark:bg-gray-800 dark:border-yellow-700 p-3">
             <a href="#">
-                <img className="rounded-t-lg" src={imageUrl} alt="" />
+                <img src={imageUrl} alt="" />
             </a>
+                  <Link href={href} passHref className="text-center">
+                    <h5 className="my-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+                </Link>
             <div className="p-5">
-                <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-                </a>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
-                <div>
-                    <ButtonArrow></ButtonArrow>
+                <div className="flex gap-3 my-3">
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
+                    <img className="w-6" src="https://cdn-icons-png.flaticon.com/512/8600/8600557.png" alt="" />{description}
                 </div>
+                <div className="flex gap-3 my-3">
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
+                    <img className="w-6" src="https://cdn-icons-png.flaticon.com/512/3816/3816653.png" alt="" />{description}
+                </div>
+                <div className="flex gap-3 my-3">
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
+                    <img className="w-6" src="https://cdn-icons-png.flaticon.com/512/1040/1040005.png" alt="" />{description}
+              </div>
+                <div className="flex gap-3 my-3">
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
+                    <img className="w-6" src="https://cdn-icons-png.flaticon.com/512/3697/3697758.png" alt="" />{description + ' €'}
+                </div>
+                <ul className="flex justify-center">
+                    <li className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-400 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <Link href={href} passHref>
+                            Prendre ma place
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </div>
     );
