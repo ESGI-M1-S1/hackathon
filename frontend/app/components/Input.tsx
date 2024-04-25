d520fbe5bd73e1561e5dc9f1bfb923ab470ea455
antoine-falgiglio <antoine.falgiglio@bedrockstreaming.com>
2024-04-25 16:24:46 +0200

    end input

--- frontend/app/components/Input.tsx
+++ frontend/app/components/Input.tsx
@@ -5,7 +5,7 @@ const InputField = () => (
   <input
     name="firstName"
     placeholder="input text"
-    className="border-brown border-2 bg-transparent h-10 mb-2 pl-4 pr-4 placeholder-white"
+    className="border-brown border bg-transparent h-10 mb-2 pl-4 pr-4 placeholder-white"
   />
 );
 
