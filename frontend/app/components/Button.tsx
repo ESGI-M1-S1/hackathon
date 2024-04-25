bde1278b5f1661901ee13589970fa9c29e5ae231
mateocarciu <carciu98@gmail.com>
2024-04-25 16:44:41 +0200

    fin cards

--- frontend/app/components/Button.tsx
+++ frontend/app/components/Button.tsx
@@ -1,15 +1,21 @@
-"use client";
 import React from "react";
 
-const Button = () => {
+type ButtonProps = {
+  text: string;
+  link: string;
+};
+
+const Button: React.FC<ButtonProps> = ({ text, link }) => {
   return (
     <div>
-      <button
-        type="button"
-        className="bg-brown text-white py-3 px-10 m-2 hover:bg-orange-300 duration-100 hover:text-white"
-      >
-        Label
-      </button>
+      <a href={link}>
+        <button
+          type="button"
+          className="bg-brown text-white py-3 px-10 m-2 hover:bg-orange-300 duration-100 hover:text-white"
+        >
+          {text}
+        </button>
+      </a>
     </div>
   );
 };
--- frontend/app/workshop/page.tsx
+++ frontend/app/workshop/page.tsx
@@ -1,66 +1,80 @@
 import React from 'react';
-import { Pagination } from 'flowbite-react';
+import { Card, Pagination } from 'flowbite-react';
 import Link from 'next/link';
-import Card from '../../components/ui/card';
 
 const cardsData = [
     {
         title: 'Card 1',
         description: 'Description 1',
-        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/1920_St_Johann_Sepia_Aquarell.jpg/1200px-1920_St_Johann_Sepia_Aquarell.jpg'
+        imgSrc: 'https://images.unsplash.com/photo-1497215728101-856f4ea42174?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         title: 'Card 2',
         description: 'Description 2',
-        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/1920_St_Johann_Sepia_Aquarell.jpg/1200px-1920_St_Johann_Sepia_Aquarell.jpg'
+        imgSrc: 'https://images.unsplash.com/photo-1497215728101-856f4ea42174?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         title: 'Card 3',
         description: 'Description 3',
-        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/1920_St_Johann_Sepia_Aquarell.jpg/1200px-1920_St_Johann_Sepia_Aquarell.jpg'
+        imgSrc: 'https://images.unsplash.com/photo-1497215728101-856f4ea42174?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     }
     ,
     {
         title: 'Card 3',
         description: 'Description 3',
-        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/1920_St_Johann_Sepia_Aquarell.jpg/1200px-1920_St_Johann_Sepia_Aquarell.jpg'
+        imgSrc: 'https://images.unsplash.com/photo-1497215728101-856f4ea42174?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     }
     ,
     {
         title: 'Card 3',
         description: 'Description 3',
-        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/1920_St_Johann_Sepia_Aquarell.jpg/1200px-1920_St_Johann_Sepia_Aquarell.jpg'
+        imgSrc: 'https://images.unsplash.com/photo-1497215728101-856f4ea42174?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         title: 'Card 3',
         description: 'Description 3',
-        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/1920_St_Johann_Sepia_Aquarell.jpg/1200px-1920_St_Johann_Sepia_Aquarell.jpg'
+        imgSrc: 'https://images.unsplash.com/photo-1497215728101-856f4ea42174?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         title: 'Card 3',
         description: 'Description 3',
-        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/1920_St_Johann_Sepia_Aquarell.jpg/1200px-1920_St_Johann_Sepia_Aquarell.jpg'
+        imgSrc: 'https://images.unsplash.com/photo-1497215728101-856f4ea42174?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         title: 'Card 3',
         description: 'Description 3',
-        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/1920_St_Johann_Sepia_Aquarell.jpg/1200px-1920_St_Johann_Sepia_Aquarell.jpg'
+        imgSrc: 'https://images.unsplash.com/photo-1497215728101-856f4ea42174?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         title: 'Card 3',
         description: 'Description 3',
-        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/1920_St_Johann_Sepia_Aquarell.jpg/1200px-1920_St_Johann_Sepia_Aquarell.jpg'
+        imgSrc: 'https://images.unsplash.com/photo-1497215728101-856f4ea42174?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     }
 ];
 
 const Page = () => {
     return (
-        <div className="flex flex-col items-center bg-black pb-3 pt-3">
-            <h1 className="text-3xl font-bold mb-4 mx-6 text-brownText">Participer aux ateliers</h1>
-            <p className="text-lg mb-4 mx-6 text-brownText">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, repudiandae necessitatibus</p>
+        <div className="flex flex-col items-center">
+            <h1 className="text-3xl font-bold mb-4 mx-6">Participer aux ateliers</h1>
+            <p className="text-lg mb-4 mx-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, repudiandae necessitatibus</p>
             <div className="flex flex-wrap justify-center gap-6">
                 {cardsData.map((card, index) => (
-                    <Card key={index} href={`/workshop/${index + 1}`} imageUrl={card.imgSrc} title={card.title} description={card.description} className='max-w-xs max-h-xs mx-6'></Card>
+                    <Card key={index} imgSrc={card.imgSrc} className='max-w-xs max-h-xs mx-6'>
+                        <div className="p-5">
+                            <Link href={`/workshop/${index + 1}`} passHref>
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{card.title}</h5>
+                            </Link>
+                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{card.description}</p>
+                            <Link href={`/workshop/${index + 1}`} passHref>
+                                <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Read more
+                                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                    </svg>
+                                </div>
+                            </Link>
+                        </div>
+                    </Card>
                 ))}
             </div>
         </div>
